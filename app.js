2b217ce3afffd5a9fadc8485b4117dc0c381a35e
prakashrnale <prakashcsedyp@gmail.com>
2019-12-31 14:27:00 +0530

    Created Express server and used middleware here

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http'); // import this module to create server
 //const fs = require('fs'); // import this module to work with file system
-const routeHandler = require('./routes');
-console.log(routeHandler.someText);
-const server = http.createServer(routeHandler.handler);
+//const routeHandler = require('./routes'); // We dont need this router handler with express
+
+const express = require('express');
+
+const app = express();
+
+app.use((req , res , next) => {
+    console.log('This is middleware one, You can modify req and res objects here');
+    //next(); // Without this controll will not reach to next middleware and req will be left hanged
+});
+
+app.use((req , res , next) => {
+    console.log('This is middleware Two, You can modify req and res objects here');
+    next(); // Without this controll will not reach to next middleware and req will be left hanged
+});
+const server = http.createServer(app);
 
 server.listen(5000);
