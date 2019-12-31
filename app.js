9262aea2d0b93f663fb4d03cba77928f97b0ae4c
prakashrnale <prakashcsedyp@gmail.com>
2019-12-31 14:45:11 +0530

    Added response in middleware

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const http = require('http'); // import this module to create server
+//const http = require('http'); // import this module to create server
 //const fs = require('fs'); // import this module to work with file system
 //const routeHandler = require('./routes'); // We dont need this router handler with express
 
 const express = require('express');
 
-const app = express();
+const app = express(); // Express is a functin we are assiging to app
 
 app.use((req , res , next) => {
     console.log('This is middleware one, You can modify req and res objects here');
-    //next(); // Without this controll will not reach to next middleware and req will be left hanged
+    next(); // Without this controll will not reach to next middleware and req will be left hanged
 });
 
 app.use((req , res , next) => {
     console.log('This is middleware Two, You can modify req and res objects here');
-    next(); // Without this controll will not reach to next middleware and req will be left hanged
+    //Here default header content type is text/html so we dont need to set it. We can any time override it
+    res.send('<h1>Hello from Express JS</h1>');
 });
-const server = http.createServer(app);
+//const server = http.createServer(app);
+//.listen(5000);
 
-server.listen(5000);
+app.listen(5000); // This listen method had internally implemented above two lines of code
