526a6766a3be85bb2e7caf1521196dcd18d4f0dc
prakashrnale <prakashcsedyp@gmail.com>
2019-12-31 16:19:24 +0530

    Added user using new route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //const http = require('http'); // import this module to create server
-//const fs = require('fs'); // import this module to work with file system
+const fs = require('fs'); // import this module to work with file system
 //const routeHandler = require('./routes'); // We dont need this router handler with express
-
+const  bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express(); // Express is a functin we are assiging to app
@@ -17,15 +17,26 @@ const app = express(); // Express is a functin we are assiging to app
 //     res.send('<h1>Hello from Express JS</h1>');
 // });
 
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/',(req , res , next)=>{
     console.log('Use this middleware for all the routes');
     next();
 })
 
-app.use('/getUsers',(req , res , next) => {
-    console.log('This is middleware one, You can modify req and res objects here');
-    res.send('<h1>This is list of all users</h1>');
+app.use('/addUsers',(req , res , next) => {
+    res.send('<form action="/users" method="POST"><input type="text" name ="user"><button type="submit">Add User</button></form>');
 });
+
+app.use('/users',(req , res , next) => {
+    console.log(req.body);
+    let newUser = req.body.user;
+    fs.writeFile('user.txt', newUser , error=>{
+        res.redirect('/');
+    });
+    
+});
+
 app.use('/',(req , res , next) => {
     console.log('This is middleware two, You can modify req and res objects here');
     res.send('<h1>This is a default page</h1>');
