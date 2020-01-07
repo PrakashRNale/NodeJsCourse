42fa7f9749925d280290bc76c4b7a44a92e5682f
prakashrnale <prakashcsedyp@gmail.com>
2020-01-07 11:01:31 +0530

    Implementated template engine

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const adminRoutes = require('./src/routes/admin');
 const shooRoutes = require('./src/routes/shop');
 
 const app = express(); // Express is a functin we are assiging to app
-
+app.set('view engine','ejs');
+app.set('views','src/views');
 app.use(bodyParser.urlencoded({extended:false}));
 // follwoing express.static function allows us to server files statically. Here we are serving css files statically.
 // Statically means not using express routing
 app.use(express.static(path.join(__dirname,'src', 'public'))); 
-app.use("/admin",adminRoutes);
+app.use("/admin",adminRoutes.router);
 app.use(shooRoutes);
 
 app.use((req , res , next) => {
-    res.status(404).sendFile(path.join(__dirname , 'src' , 'views' , 'not-found.html'));
+    // res.status(404).sendFile(path.join(__dirname , 'src' , 'views' , 'not-found.html'));
+    res.render('404',{pageTitle : 'Page Not Found'});
 });
 
 app.listen(5000); // This listen method had internally implemented above two lines of code
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const products = require('./admin');
 const path = require('path');
 const rootDir = require('../util/path');
 
 router.get('/',(req , res , next) => {
     console.log('This is middleware two, You can modify req and res objects here');
-    res.sendFile(path.join(rootDir ,'src', 'views' , 'shop.html'));
+    // res.sendFile(path.join(rootDir ,'src', 'views' , 'shop.html'));
+    console.log(products.product);
+    res.render('shop',{pageTitle : 'Product List' , prods : products.product });
 });
 
 module.exports = router;
