ff2bdb225668dcaeca814e401355200b457b0a27
prakashrnale <prakashcsedyp@gmail.com>
2020-01-07 12:22:18 +0530

    Created Controller and models

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
 const router = express.Router();
+const productController = require('../controllers/product');
 
-const product = [];
 // /admin/add-product  -- GET
-router.get('/add-product',(req , res , next) => {
-    //res.sendFile(path.join(__dirname , '../' , 'views' , 'add-product.html'));
-    // or we can use more clean way of gatting root path
-    // res.sendFile(path.join(rootDir ,'src', 'views' , 'add-product.html'));
-    res.render('add-product',{pageTitle : 'Page Not Found'});
-});
+router.get('/add-product',productController.getAddProduct);
 
 
 // /admin/add-product  -- POST
-router.post('/add-product',(req , res , next) => { 
-    let newProduct = req.body.title;
-    product.push({product : newProduct});
-    console.log(`new product is ${newProduct}`);
-    res.redirect('/'); // status code will be automatically get attched to it
-});
+router.post('/add-product',productController.postAddProduct);
 
 module.exports = {
-    router : router,
-    product : product
+    router : router
 };
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const products = require('./admin');
-const path = require('path');
-const rootDir = require('../util/path');
+const productController = require('../controllers/product');
 
-router.get('/',(req , res , next) => {
-    console.log('This is middleware two, You can modify req and res objects here');
-    // res.sendFile(path.join(rootDir ,'src', 'views' , 'shop.html'));
-    console.log(products.product);
-    res.render('shop',{pageTitle : 'Product List' , prods : products.product });
-});
+
+router.get('/',productController.getProducts);
 
 module.exports = router;
